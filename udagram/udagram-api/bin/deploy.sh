87e60a339620fea4beca13505115735a90431afe
Ahmedghoraba99 <ahmed.12.2011@gmail.com>
2022-11-28 11:17:46 +0200

    fixed scripts

---
+++ udagram/udagram-api/bin/deploy.sh
@@ -0,0 +1,11 @@
+eb setenv AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --profile default
+eb setenv AWS_BUCKET=$AWS_BUCKET --profile default
+eb setenv AWS_REGION=$AWS_DEFAULT_REGION --profile default
+eb setenv SECRET_ACCESS_KEY_ID=$AWS_SECRET_ACCESS_KEY --profile default
+eb setenv JWT_SECRET=$JWT_SECRET --profile default
+eb setenv POSTGRES_DB=$POSTGRES_DB --profile default
+eb setenv POSTGRES_HOST=$POSTGRES_HOST --profile default
+eb setenv POSTGRES_PASSWORD=$POSTGRES_PASSWORD --profile default
+eb setenv POSTGRES_PORT=$POSTGRES_PORT --profile default
+eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME --profile default
+  
