7a7b612b27ba4ef05c362fa1995008207c464219
Ahmedghoraba99 <ahmed.12.2011@gmail.com>
2022-11-28 16:49:57 +0200

    fixed backend api

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://udagramtest.eba-zpbvsrha.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
